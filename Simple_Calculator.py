7881a623837e3ed559546c4ff004b5ab5c0f3cb9
archismandas96 <archismandas96@gmail.com>
2025-08-27 10:45:23 +0530

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,11 @@
+#Simple Calculator
+import streamlit as st
+st.title("Simple Calculator")
+num1= st.number_input("Enter first number")
+num2= st.number_input("Enter second number")
+operation= st.radio("Operation:",["Add","Substract"])
+if st.button("Calculate"):
+    if operation=="Add":
+        st.write("Result",num1+num2)
+    else:
+        st.write("Result",num1-num2)    
