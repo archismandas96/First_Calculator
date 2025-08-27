68648c031b88a90ea588d5495e14e5eaa89cadc9
archismandas96 <archismandas96@gmail.com>
2025-08-27 11:07:36 +0530

    Update Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,11 +1,22 @@
 #Simple Calculator
 import streamlit as st
-st.title("Simple Calculator")
-num1= st.number_input("Enter first number")
-num2= st.number_input("Enter second number")
-operation= st.radio("Operation:",["Add","Substract"])
-if st.button("Calculate"):
-    if operation=="Add":
-        st.write("Result",num1+num2)
+st.title("Calculator")
+num1= st.number_input("Enter the first number")
+num2= st.number_input("Enter the second number")
+
+selection = st.selectbox("Select",["Add","Substract","Multiplication","Division"])
+result = None
+if st.button("Submit"):
+    if selection =="Add":
+        result = num1 + num2
+    elif selection =="Substract":
+        result = num1 - num2
+    elif selection =="Multiplication":
+        result = num1 * num2
+    elif selection =="Division":
+        result = num1/num2
     else:
-        st.write("Result",num1-num2)    
+        result = None
+        
+if result:
+    st.success(f"Result is:  {result}" )   
